237a205b150bfc5f4e96ffa98a60c7154c9d0799
xcvdbals666 <xcvdbals666@naver.com>
2025-12-22 17:40:05 +0900

    .

--- back/routers/usersRouter.js
+++ back/routers/usersRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const usersService = require("../services/usersSerivce");
+const usersService = require("../services/usersService");
 
 // 로그인
 router.post("/login", async (req, res) => {
