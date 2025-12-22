3b32daeaf59e2ee47cec72441dda1d7f21f35c38
jay <youngjin2712@naver.com>
2025-12-22 16:15:40 +0900

    J_20251222_개발표준추가

--- back/routers/usersRouter.js
+++ back/routers/usersRouter.js
@@ -3,7 +3,17 @@ const router = express.Router();
 const usersService = require("../services/usersService.js");
 
 // 로그인
-router.get("/login", async (req, res) => {
-  let list = await usersService.findByIdAndPw();
+router.post("/login", async (req, res) => {
+  const { id, pw } = req.body;
+  let list = await usersService.findByIdAndPwUsers(id, pw);
   res.send(list);
 });
+
+// 아이디 찾기
+router.post("/findId", async (req, res) => {
+  const { name, email } = req.body;
+  let list = await usersService.findByNameAndEmailUsers(name, email);
+  res.send(list);
+});
+
+module.exports = router;
