fbbb1840e8be6b127a6f2857cdff60f537f0a99e
oseongnuna <7040101a@gmail.com>
2025-12-22 12:09:17 +0900

    N_20251222_템플릿적용

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,11 +1,40 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div :class="{ 'nav-open': $sidebar.showSidebar }">
+    <notifications></notifications>
+    <router-view></router-view>
+  </div>
 </template>
 
-<style scoped></style>
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.vue-notifyjs.notifications {
+  .alert {
+    z-index: 10000;
+  }
+  .list-move {
+    transition: transform 0.3s, opacity 0.4s;
+  }
+  .list-item {
+    display: inline-block;
+    margin-right: 10px;
+  }
+  .list-enter-active {
+    transition: transform 0.2s ease-in, opacity 0.4s ease-in;
+  }
+  .list-leave-active {
+    transition: transform 1s ease-out, opacity 0.4s ease-out;
+  }
+
+  .list-enter {
+    opacity: 0;
+    transform: scale(1.1);
+  }
+  .list-leave-to {
+    opacity: 0;
+    transform: scale(1.2, 0.7);
+  }
+}
+</style>
